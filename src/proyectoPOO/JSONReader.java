c365ed07af736c156f9cb18c3debe2c7ab917a44
Mauricio Arrieta <mauarrieta24@gmail.com>
2021-04-13 22:58:17 -0600

    creacion automatica de pedidos desde ListaMenu.items

--- src/proyectoPOO/JSONReader.java
+++ src/proyectoPOO/JSONReader.java
@@ -3,7 +3,7 @@ package proyectoPOO;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.List;
+//import java.util.List;
 import java.util.Random;
 
 import org.json.simple.parser.*;
@@ -12,18 +12,19 @@ import org.json.simple.*;
 public class JSONReader {
 	
 	JSONParser parser;
-	List fuertes;
-	List acompanamientos;
-	List bebidas;
+	//List fuertes;
+	//List acompanamientos;
+	//List bebidas;
 	
-	public JSONReader() {
+	
+	public JSONReader(Menu lista) {
 		parser = new JSONParser();
 		
 		try {
 			// Crea un objeto tipo JSON que contiene toda la información
 			JSONObject menu = (JSONObject) parser.parse(new FileReader("Menu.json"));
 			//System.out.println(menu);
-			extraerInfo(menu); // Extrae toda la información del JSON
+			extraerInfo(menu,lista); // Extrae toda la información del JSON
 		}
 		// En caso se que se encuentre algun error
 		catch(FileNotFoundException e) {e.printStackTrace();}
@@ -32,7 +33,8 @@ public class JSONReader {
 		catch(Exception e) {e.printStackTrace();}
 	}
 	
-	private static void extraerInfo(JSONObject m) {
+	private static void extraerInfo(JSONObject m,Menu listaMenu) {
+		
 		// Extrae la información de todos los platos fuertes
 		JSONArray plato = (JSONArray) m.get("Plato fuerte"); // Cuando en el JSON tiene paréntesis cuadrados, se usan los JSONArrays
 		//System.out.println(plato);
@@ -43,27 +45,27 @@ public class JSONReader {
 			
 			//System.out.println(platillo.get("Nombre"));
 			f1.nombre = platillo.get("Nombre").toString();
-			System.out.println(f1.nombre);
+			//System.out.println(f1.nombre);
 			
 			//System.out.println(platillo.get("Precio"));
 			f1.precio = Integer.valueOf(platillo.get("Precio").toString());
-			System.out.println(f1.precio);
+			//System.out.println(f1.precio);
 			
 			//System.out.println(platillo.get("Duracion"));
 			f1.tiempoProduccion = Integer.valueOf(platillo.get("Duracion").toString());
-			System.out.println(f1.tiempoProduccion);
+			//System.out.println(f1.tiempoProduccion);
 			
 			JSONArray tamano = (JSONArray) platillo.get("Tamano");
 			Random random = new Random();
 			int aleatorio = random.nextInt(2);
 			f1.tamanno = tamano.get(aleatorio).toString();
-			System.out.println(f1.tamanno);
+			//System.out.println(f1.tamanno);
 			
 			//for (int i2 = 0; i2 < tamano.size(); i2++) {
 				//System.out.println(tamano.get(i2));
 			//}
-			
-		System.out.println("\n -------- \n ");
+		listaMenu.items.add(f1);
+		//System.out.println("\n -------- \n ");
 		}
 		// Extrae la información de todos los acompañamientos
 		
@@ -76,30 +78,28 @@ public class JSONReader {
 			
 			//System.out.println(platillo.get("Nombre"));
 			a1.nombre = platillo.get("Nombre").toString();
-			System.out.println(a1.nombre);
+			//System.out.println(a1.nombre);
 			
 			//System.out.println(platillo.get("Precio"));
 			a1.precio = Integer.valueOf(platillo.get("Precio").toString());
-			System.out.println(a1.precio);
+			//System.out.println(a1.precio);
 			
 			//System.out.println(platillo.get("Duracion"));
 			a1.tiempoProduccion = Integer.valueOf(platillo.get("Duracion").toString());
-			System.out.println(a1.tiempoProduccion);
+			//System.out.println(a1.tiempoProduccion);
 			
 			JSONArray tipo = (JSONArray) platillo.get("Tipo");
 			Random random1 = new Random();
 			int indice = random1.nextInt(2);
 			a1.tipo = tipo.get(indice).toString();
-			System.out.println(a1.tipo);
-			/*
-			for (int i2 = 0; i2 < tipo.size(); i2++) {
-				System.out.println(tipo.get(i2));
-			}*/
-			System.out.println(" \n ------- \n ");
+			//System.out.println(a1.tipo);
+			
+			listaMenu.items.add(a1);
+			//System.out.println(" \n ------- \n ");
 		}
 		// Extrae la información de todas las bebidas
 		JSONArray plato3 = (JSONArray) m.get("Bebidas");
-		//System.out.println(plato3);
+		
 		for (int i = 0; i < plato.size(); i++) {
 			Bebida b1 = new Bebida();
 			b1.espacio = 1;//siempre ocupan 1 espacio...
@@ -107,33 +107,34 @@ public class JSONReader {
 			
 			//System.out.println(platillo.get("Nombre"));
 			b1.nombre = platillo.get("Nombre").toString();
-			System.out.println(b1.nombre);
+			//System.out.println(b1.nombre);
 			
 			//System.out.println(platillo.get("Precio"));
 			b1.precio = Integer.valueOf(platillo.get("Precio").toString());
-			System.out.println(b1.precio);
+			//System.out.println(b1.precio);
 			
 			//System.out.println(platillo.get("Duracion"));
 			b1.tiempoProduccion = Integer.valueOf(platillo.get("Duracion").toString());
-			System.out.println(b1.tiempoProduccion);
+			//System.out.println(b1.tiempoProduccion);
 			
 			JSONArray tipo = (JSONArray) platillo.get("Tipo");
 			Random random2 = new Random();
 			int indice = random2.nextInt(2);
 			b1.tipo = tipo.get(indice).toString();
-			System.out.println(b1.tipo);
+			//System.out.println(b1.tipo);
 			
 			JSONArray tamano = (JSONArray) platillo.get("Tamano");
 			Random random3 = new Random();
 			int indice1 = random3.nextInt(3);
 			b1.tamanno = Integer.valueOf(tamano.get(indice1).toString());
-			System.out.println(b1.tamanno);
-			
-			System.out.println(" \n -------- \n");
+			//System.out.println(b1.tamanno);
+			listaMenu.items.add(b1);
+			//System.out.println(" \n -------- \n");
 		}
+		//System.out.println(listaMenu.items);
 		
 	}
-	
+	/*
 	public List platosFuerte() {
 		return fuertes;
 	}
@@ -144,5 +145,5 @@ public class JSONReader {
 	
 	public List bebidas() {
 		return bebidas;
-	}
+	}*/
 }
--- src/proyectoPOO/Main.java
+++ src/proyectoPOO/Main.java
@@ -1,13 +1,15 @@
 package proyectoPOO;
-//import java.util.Scanner; para inputs
+import java.util.Scanner;// para inputs
 import java.util.Iterator;
+import java.util.Random;
 
 public class Main {
 	
-	//global //
+	// global //
 	
 	static Cola colaClientes = new Cola();
 	static ColaProduccion colaProduc = new ColaProduccion();
+	static Menu ListaMenu = new Menu();//ListaMenu.items contiene arrayList con todos los alimentos del restaurante
 	
 	static int ordenesCompletadas =0;
 	
@@ -19,6 +21,22 @@ public class Main {
 			}
 		}
 	return condicion;
+	}
+	
+	public static void crearPedido() {
+		Pedido pedido = new Pedido();
+		Random r1 = new Random();
+		int aleatorio = r1.nextInt(5);//cuantos productos como maximo? puse 5 (cuenta aun cuando llega a 0)
+		
+		while(aleatorio>=0) {
+			Random r2 = new Random();
+			int r3 = r2.nextInt(12);//cualquier objeto <Alimento> dentro de ListaMenu estara en este indice
+			pedido.items.add(ListaMenu.items.get(r3));
+			//System.out.println(r3);
+			aleatorio--;
+		}
+		System.out.println(pedido.items);
+		colaClientes.pedidosTotales.add(pedido);
 	}
 	
 	public static void avanzar() {
@@ -72,82 +90,20 @@ public class Main {
 	}//fin de avanzar()
 
 	public static void main(String[] args) {
-		//luego de crear instancias de JSON, se meten en array 
-		// de ese array agarramos esos objetos para meterlos a instancias pedido.
-		JSONReader fileJSON = new JSONReader();
+		//luego de crear instancias de JSON, se meten en array (ListaMenu)
+		// de ese array agarramos esos objetos <Alimento> para meterlos a instancias Pedido.
 		
-		/*
-		Bebida b1 = new Bebida();
-		b1.nombre = "Guanabana";
-		b1.precio = 1000 ;
-		b1.tiempoProduccion = 2;
-		b1.tipo="Frio";
-		b1.tamanno = 250;
-		b1.espacio = 1;
-		
-		Bebida b2 = new Bebida();
-		b2.nombre = "coca";
-		b2.precio = 1000 ;
-		b2.tiempoProduccion = 2;
-		b2.tipo="Frio";
-		b2.tamanno = 250;
-		b2.espacio = 9;
-		
-		Bebida b3 = new Bebida();
-		b3.nombre = "fresa_en_agua";
-		b3.precio = 1000 ;
-		b3.tiempoProduccion = 2;
-		b3.tipo="Frio";
-		b3.tamanno = 250;
-		b3.espacio = 1;
-		
-		Pedido p1 = new Pedido();
-		p1.items.add(b1);
-		
-		Pedido p2 = new Pedido();
-		p2.items.add(b2);
-		p2.items.add(b3);
-		
-		colaClientes.pedidosTotales.add(p1);
-		colaClientes.pedidosTotales.add(p2);
-		
-		System.out.println(colaProduc.espacioOcupado());
-		System.out.println("\n ---- totales: \n");
-		System.out.println(colaClientes.pedidosTotales);
-		avanzar();
-		System.out.println("\n----  1 --------  \n");
-		System.out.println(colaProduc.espacioOcupado());
-		System.out.println(colaProduc.produccion);
-		System.out.println("\n ---- totales: \n");
-		System.out.println(colaClientes.pedidosTotales);
-		System.out.println("\n //////////////////////////////// \n");
-		avanzar();
-		System.out.println("\n---------- 2 ----------\n");
-		System.out.println(colaProduc.espacioOcupado());
-		System.out.println(colaProduc.produccion);
-		System.out.println("\n ---- totales: \n");
-		System.out.println(colaClientes.pedidosTotales);
-		System.out.println("\n //////////////////////////////// \n");
-		avanzar();
-		System.out.println("\n-------- 3 ---------\n");
-		System.out.println(colaProduc.espacioOcupado());
-		System.out.println(colaProduc.produccion);
-		System.out.println("\n ---- totales: \n");
-		System.out.println(colaClientes.pedidosTotales);
-		System.out.println("\n //////////////////////////////// \n");
-		avanzar();
-		System.out.println("\n-------- 3 ---------\n");
-		System.out.println(colaProduc.espacioOcupado());
-		System.out.println(colaProduc.produccion);
-		System.out.println("\n ---- totales: \n");
-		System.out.println(colaClientes.pedidosTotales);
-		System.out.println("\n //////////////////////////////// \n");
-		
+		JSONReader fileJSON = new JSONReader(ListaMenu);
+		//System.out.println(ListaMenu.items);
 		
 		System.out.println("Digite la cantidad de clientes por crear: ");
 		Scanner scan = new Scanner(System.in);
 		int cantidad = scan.nextInt();
-		*/
+		
+		while (cantidad>0) {
+			crearPedido();
+			cantidad--;
+		}
 		
 	}
 
